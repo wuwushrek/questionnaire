2bd176ad005f3f299ab64a9f164a8c53d94fcdd0
wuwushrek <frmbouwe@gmail.com>
2016-02-15 14:26:54 +0100

    mise a jour bootstrap/ ajout id-mdp

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,36 @@
-class BootStrap {
+import questionnaire.Direction
+import questionnaire.Eleve
 
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+class BootStrap {
+	def init = { servletContext ->
+		environments {
+			def development = development {
+
+				def admin = new Direction(nom:'admin', mdp:'sorbetcitron'); // un seul admin
+				admin.save();
+				if(admin.hasErrors()) {
+					println admin.getErrors();
+				}
+
+				Eleve[] user = new Eleve[6];
+
+				user[0] = new Eleve(nom:'alix', mdp:'alix');
+				user[1] = new Eleve(nom:'leaLeCam', mdp:'lealecam');
+				user[2] = new Eleve(nom:'antoine', mdp:'antoine');
+				user[3] = new Eleve(nom:'franck', mdp:'franck');
+				user[4] = new Eleve(nom:'roman', mdp:'roman');
+				user[5] = new Eleve(nom:'zakaria', mdp:'zakaria');
+
+				for(int i = 0; i < 6; i++)
+				{
+					user[i].save();
+					if(user[i].hasErrors()) {
+						println user[i].getErrors();
+					}
+				}
+			}
+		}
+	}
+	def destroy = {
+	}
 }
